491389d8ea427d434ef16492e13aacac12445575
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-26 23:39:46 -0400

    build updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,43 @@
   pkg = require("./package.json"),
   build = require("notiontheory-basic-build"),
   nt = build.setup(gulp, pkg),
-  js = nt.js(pkg.name, "src", ["format"]),
+  pre = nt.js("pre", "pre"),
+  js = nt.js(pkg.name + "Lib", "src", ["format"]),
+  tot = nt.cat(pkg.name, [
+    "../primrose/Primrose.js",
+    pkg.name + "Lib.js"
+  ], [js]),
   html = nt.html(pkg.name, ["*.pug"]),
   css = nt.css(pkg.name, ["*.styl"]);
 
 gulp.task("format", [js.format]);
 
 gulp.task("default", [
+  pre.default,
   js.default,
   html.default,
   css.default
 ]);
 
 gulp.task("debug", [
-  js.debug,
+  pre.debug,
+  tot.debug,
   html.debug,
   css.debug
 ]);
 
 gulp.task("test", [
-  js.release,
+  pre.release,
+  tot.release,
   html.test,
   css.release
 ]);
 
 gulp.task("release", [
-  js.release,
+  pre.release,
+  tot.release,
   html.release,
   css.release
 ]);
+
+gulp.task("kablamo", build.exec("npm update && gulp bump && gulp yolo && gulp trololo"));
