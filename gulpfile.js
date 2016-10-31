6b4c0ff5c53be5ce45ba8b5dfab578516c295017
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-31 12:04:48 -0400

    add back dependencies that Primrose removed

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 ﻿var gulp = require("gulp"),
   pkg = require("./package.json"),
-  build = require("notiontheory-basic-build"),
+  build = require("../notiontheory-basic-build"),
   nt = build.setup(gulp, pkg),
   pre = nt.js("pre", "pre"),
   js = nt.js(pkg.name + "Lib", "src", ["format"]),
   tot = nt.cat(pkg.name, [
+    "node_modules/jshashes/hashes.js",
+    "node_modules/socket.io-client/socket.io.js",
+    "lib/namegen.js",
     "../primrose/Primrose.js",
     pkg.name + "Lib.js"
   ], [js]),
   html = nt.html(pkg.name, ["*.pug"]),
-  css = nt.css(pkg.name, ["*.styl"]);
+  css = nt.css(pkg.name, ["*.styl"]),
+  clean = nt.clean(pkg.name, ["*Lib*.js"], [tot.release]);
 
 gulp.task("format", [js.format]);
 
 gulp.task("default", [
   pre.default,
   js.default,
+  tot.default,
   html.default,
   css.default
 ]);
@@ -36,7 +41,7 @@ gulp.task("test", [
 
 gulp.task("release", [
   pre.release,
-  tot.release,
+  clean,
   html.release,
   css.release
 ]);
