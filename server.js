a390aca2a694473029afc56ffe628f10378b5753
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-05-12 17:23:35 -0400

    ugh

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("marigold-build/src/starters/server")({
   path: ".",
+  port: process.env.PORT,
   webSocketServer: require("./server/webSocketServer"),
   express: require("./server/routes")
 });
