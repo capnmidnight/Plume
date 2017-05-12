37d70aafaa99dcb3363d0f3fcf76093402d77503
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-05-12 16:37:19 -0400

    Remove extraneous key spec

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 require("marigold-build/src/starters/server")({
   path: ".",
-  key: "../primrosevr_com.key",
-  cert: "../primrosevr_com.crt",
-  certAuthority: "../CACert.crt",
   webSocketServer: require("./server/webSocketServer"),
   express: require("./server/routes")
 });
