d3196dd69c3f9b05f13f8feb6203a83ab7ddf544
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-05-14 22:57:11 -0400

    back to testing one instance

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@
   }),
   html = marigold.html(["*.pug"]),
   css = marigold.css(["*.styl"]),
+  testNames = ["Sean", "Dave"],
   devServer = marigold.devServer([
     "src/**/*",
     "*.pug",
@@ -21,6 +22,12 @@
       mode: "dev"
     }),
     mode: "dev",
+    path: ".",
+    url: "index.html",
+    // instances: 2,
+    // instance: function(i) {
+    //   return "?room=NotionTheory&user=" + testNames[i];
+    // },
     key: "../primrosevr_com.key",
     cert: "../primrosevr_com.crt",
     certAuthority: "../CACert.crt",
