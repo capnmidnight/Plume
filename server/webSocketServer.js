99cdcac02005e142edff67c97d866da1c1459259
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-05-15 14:58:17 -0400

    these things don't need to be state-specific

--- server/webSocketServer.js
+++ server/webSocketServer.js
@@ -1,87 +1,94 @@
 "use strict";
 
-module.exports = function(options) {
+const User = require("./data/User"),
+  activeUsers = {};
 
-  const User = require("./data/User"),
-    activeUsers = {},
-    isDev = options && options.mode === "dev" || process.env.NODE_ENV === "dev";
-
-  function broadcast(evt) {
-    for (var key in activeUsers) {
-      var toUser = activeUsers[key];
-      if (toUser.appKey === evt.user.appKey) {
-        toUser.emit
-          .bind(toUser, (toUser.userName === evt.user.userName) ? evt.skipSocketIndex : -1)
-          .apply(toUser, evt.args);
-      }
+function broadcast(evt) {
+  for (var key in activeUsers) {
+    var toUser = activeUsers[key];
+    if (toUser.appKey === evt.user.appKey) {
+      toUser.emit
+        .bind(toUser, (toUser.userName === evt.user.userName) ? evt.skipSocketIndex : -1)
+        .apply(toUser, evt.args);
     }
   }
+}
 
-  function peer(evt) {
-    var fromUserKey = User.makeID({
-        userName: evt.fromUserName,
-        appKey: evt.appKey
-      }),
-      toUserKey = User.makeID({
-        userName: evt.toUserName,
-        appKey: evt.appKey
-      }),
-      fromUser = activeUsers[fromUserKey],
-      toUser = activeUsers[toUserKey];
+function peer(evt) {
+  var fromUserKey = User.makeID({
+      userName: evt.fromUserName,
+      appKey: evt.appKey
+    }),
+    toUserKey = User.makeID({
+      userName: evt.toUserName,
+      appKey: evt.appKey
+    }),
+    fromUser = activeUsers[fromUserKey],
+    toUser = activeUsers[toUserKey];
 
-    if (fromUser && toUser) {
-      var fromIndex = evt.fromUserIndex || 0,
-        toIndex = evt.toUserIndex || 0,
-        fromSocket = fromUser.devices[fromIndex],
-        toSocket = toUser.devices[toIndex];
+  if (fromUser && toUser) {
+    var fromIndex = evt.fromUserIndex || 0,
+      toIndex = evt.toUserIndex || 0,
+      fromSocket = fromUser.devices[fromIndex],
+      toSocket = toUser.devices[toIndex];
 
-      if (fromSocket && toSocket) {
-        ["offer", "answer", "ice", "cancel"].forEach((evtName) => {
-          const thunk = (obj) => {
-            toSocket.emit(evtName, obj);
-          };
-          fromUser.handlers[fromIndex][evtName] = thunk;
-          fromSocket.on(evtName, thunk);
-        });
+    if (fromSocket && toSocket) {
+      ["offer", "answer", "ice", "cancel"].forEach((evtName) => {
+        const thunk = (obj) => {
+          toSocket.emit(evtName, obj);
+        };
+        fromUser.handlers[fromIndex][evtName] = thunk;
+        fromSocket.on(evtName, thunk);
+      });
 
-        toSocket.emit("peer", evt);
-      }
-      else{
-        console.error("peer error", evt);
-      }
+      toSocket.emit("peer", evt);
+    }
+    else{
+      console.error("peer error", evt);
     }
   }
+}
 
-  function listUsers(evt){
-    if(evt.user){
-      var socket = evt.user.devices[evt.index];
-      if(socket){
-        var userList = [];
-        for (var key in activeUsers) {
-          var user = activeUsers[key];
-          if (user.isConnected &&
-            user.appKey === evt.user.appKey &&
-            (user.userName !== evt.user.userName || evt.index > 0)) {
-            userList.push(user.getPackage());
-          }
+function listUsers(evt){
+  if(evt.user){
+    var socket = evt.user.devices[evt.index];
+    if(socket){
+      var userList = [];
+      for (var key in activeUsers) {
+        var user = activeUsers[key];
+        if (user.isConnected &&
+          user.appKey === evt.user.appKey &&
+          (user.userName !== evt.user.userName || evt.index > 0)) {
+          userList.push(user.getPackage());
         }
-        socket.emit("userList", userList);
       }
+      socket.emit("userList", userList);
     }
   }
+}
 
-  function setUser(socket, identity, key, user){
-    if (!activeUsers[key]) {
-      if(user) {
-        user.appKey = identity.appKey;
-      }
-      activeUsers[key] = new User(user || identity);
-      activeUsers[key].addEventListener("broadcast", broadcast);
-      activeUsers[key].addEventListener("peer", peer);
-      activeUsers[key].addEventListener("listUsers", listUsers);
+function setUser(socket, identity, key, user){
+  if (!activeUsers[key]) {
+    if(user) {
+      user.appKey = identity.appKey;
     }
+    activeUsers[key] = new User(user || identity);
+    activeUsers[key].addEventListener("broadcast", broadcast);
+    activeUsers[key].addEventListener("peer", peer);
+    activeUsers[key].addEventListener("listUsers", listUsers);
+  }
 
-    activeUsers[key].addDevice(socket, identity.appKey);
+  activeUsers[key].addDevice(socket, identity.appKey);
+}
+
+module.exports = function(options) {
+  console.log(options);
+  const isDev = options && options.mode === "dev" || process.env.NODE_ENV === "dev";
+
+  function guestLogin(socket) {
+    return setup(socket, "login", (identity, key) => {
+      setUser(socket, identity, key);
+    });
   }
 
   function setup(socket, verb, thunk){
@@ -105,12 +112,6 @@ module.exports = function(options) {
         }
       }
     }
-  }
-
-  function guestLogin(socket) {
-    return setup(socket, "login", (identity, key) => {
-      setUser(socket, identity, key);
-    });
   }
 
   return function (socket) {
